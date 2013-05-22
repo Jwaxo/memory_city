b7874b2c68a6858ee7dfa5b09b0542b9b37cb7eb
Jwaxo <michfreak@gmail.com>
2013-05-22 16:53:30 -0700

    Separated server loading to server_init.js

--- init.js
+++ init.js
@@ -1,18 +1,17 @@
 //Basic Hello World to get at least SOMETHING in the repo that isn't a Readme.
 
 var http = require('http');
-var express = require('express');
+var server = require('./lib/modules/server_init').createServer();
+
 var seedrandom_module = require('./lib/modules/seedrandom/seedrandom.js');
 
 Math.seedrandom('seed');
 var string = Math.random().toString();
 
-server = express();
-
 server.get('/', function (request, response) {
 	response.send(string);
 });
 
-server.listen(8080);
+server.listen(8000);
 
 console.log("Server running at http://127.0.0.1:8000/");
