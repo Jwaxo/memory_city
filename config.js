818dd0c3f1fec389858dac5cc1208854a181cfa7
Jwaxo <michfreak@gmail.com>
2013-05-30 15:51:51 -0700

    Grid correction, comments, styles

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ exports.values = {
 
 	seed: 'FirstValue',
 	map: {
-		x: 10, //Highest maximum value of X. The grid will go from -x to x
-		y: 10, //Same as above for y
+		x: 5, //Highest maximum value of X. The grid will go from -x to x
+		y: 5, //Same as above for y
 		roots: 3
 	}
 
