b341037550ca3b4819d956bb431d14f9fd9920c2
Jwaxo <michfreak@gmail.com>
2013-05-30 16:33:12 -0700

    Added coordinate and root gen.

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 exports.values = {
 
-	seed: 'FirstValue',
+	seed: 'SecondValue',
 	map: {
 		x: 5, //Highest maximum value of X. The grid will go from -x to x
 		y: 5, //Same as above for y
-		roots: 3
+		roots: 3,
+		root_type: 'building'
 	}
 
 }
