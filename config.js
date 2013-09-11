449105aaf23cc295b4d5c5e2699990314c4aeddc
Jwaxo <jeff@jeffwax.com>
2013-09-10 17:23:37 -0700

    Added roots explanation.

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.values = {
     map: {
         x: 5, //Highest maximum value of X. The grid will go from -x to x
         y: 5, //Same as above for y
-        roots: 3,
+        roots: 3, //Determines basic, required buildings, which branch roads
         root_type: 'building'
     }
 
