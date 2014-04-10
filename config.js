d8eefe6c2859f17011a32fc762cdcdc580573313
Jwaxo <jeff@jeffwax.com>
2014-04-10 16:34:54 -0700

    Added todo regarding fs, some other small syntax updates.

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 exports.values = {
 
-    seed: 'Fill it ALL with adjacency.',
-    map: {
-        x: 50, //Highest maximum value of X. The grid will go from 0 to x
-        y: 50, //Same as above for y
-        roots: ['R','I','C'] //Determines initial nodes created based on zone type; also makes initial roads
-    }
+  seed: 'Fill it ALL with adjacency.',
+  map: {
+    x: 50, //Highest maximum value of X. The grid will go from 0 to x
+    y: 50, //Same as above for y
+    roots: ['R','I','C'] //Determines initial nodes created based on zone type; also makes initial roads
+  }
 
 }
--- test.js
+++ test.js
@@ -26,33 +26,33 @@ var express = require('express');
 var server = express();
 
 server.configure(function() {
-    //Begin setting up the server
-    server.set('title', 'Memory City');
-    server.set('views', __dirname + '/views');
-    server.set('view engine', 'ejs');
-    server.engine('.html', require('ejs').renderFile);
-    server.use(express.static(__dirname + '/public'));
-    server.use(express.bodyParser());
+  //Begin setting up the server
+  server.set('title', 'Memory City');
+  server.set('views', __dirname + '/views');
+  server.set('view engine', 'ejs');
+  server.engine('.html', require('ejs').renderFile);
+  server.use(express.static(__dirname + '/public'));
+  server.use(express.bodyParser());
 });
 
 server.configure('development', function() {
-    console.log('Server running in development mode.');
+  console.log('Server running in development mode.');
 });
 server.configure('production', function() {
-    console.log('Server running in production mode.');
+  console.log('Server running in production mode.');
 });
 
 var body = {
-    'map' : map
+  'map' : map
 };
 var views_var = server.get('views');
 
 server.get('/', function (request, response) {
-    console.log("Rendering view index.html at " + views_var);
-    response.render('index.html', {
-        'title': 'Memory City',
-        'body': body
-    });
+  console.log("Rendering view index.html at " + views_var);
+  response.render('index.html', {
+    'title': 'Memory City',
+    'body': body
+  });
 });
 
 server.listen(8000);
